8c5d110fe1d83f4d265eb9cd1e5dec1d2b355eb7
Shanen Cross <shanencross@yahoo.com>
2021-06-07 20:03:25 -0700

    Add deserialization of json into objects
    Co-authored-by: Salim Mayan <mailsalim@gmail.com>

--- UsingRestSharp/Program.cs
+++ UsingRestSharp/Program.cs
@@ -1,22 +1,31 @@
 using System;
 using System.Threading.Tasks;
 using RestSharp;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
 
-namespace UsingRestSharp
-{
-  public class Program
-  {
-    static void Main()
-    {
-      var apiCallTask = ApiHelper.ApiCall("[INSERT-API-KEY]");
+namespace UsingRestSharp {
+  public class Program {
+    static void Main() {
+      var apiCallTask = ApiHelper.ApiCall("[YOUR-API-KEY]");
       var result = apiCallTask.Result;
-      Console.WriteLine(result);
+      // Console.WriteLine(result);
+      JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(result);
+      // Console.WriteLine(jsonResponse["results"]);
+      List<Article> articleList = JsonConvert.DeserializeObject<List<Article>>(jsonResponse["results"].ToString());
+
+      foreach (Article article in articleList) {
+        Console.WriteLine($"Section: {article.Section}");
+        Console.WriteLine($"Title: {article.Title}");
+        Console.WriteLine($"Abstract: {article.Abstract}");
+        Console.WriteLine($"Url: {article.Url}");
+        Console.WriteLine($"Byline: {article.Byline}");
+      }
     }
 
-    class ApiHelper
-    {
-      public static async Task<string> ApiCall(string apiKey)
-      {
+    class ApiHelper {
+      public static async Task<string> ApiCall(string apiKey) {
         RestClient client = new RestClient("https://api.nytimes.com/svc/topstories/v2");
         RestRequest request = new RestRequest($"home.json?api-key={apiKey}", Method.GET);
         var response = await client.ExecuteTaskAsync(request);
